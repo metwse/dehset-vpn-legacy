bb0c6731dc736d7e271cdda386acadf8ed61760d
metwse <108795071+metwse@users.noreply.github.com>
2025-05-28 13:27:01 +0300

    tests: handshake integration test

---
+++ testutil/tests/handshake.rs
@@ -0,0 +1,15 @@
+use tokio::io::simplex;
+
+#[tokio::test]
+async fn handshake() {
+    let (mut sr, mut cw) = simplex(u16::MAX as usize);
+    let (mut cr, mut sw) = simplex(u16::MAX as usize);
+
+    let (client_handshake, server_handshake) = tokio::join! (
+        client::connection::do_handshake(&mut cr, &mut cw),
+        server::connection::do_handshake(&mut sr, &mut sw),
+    );
+
+    client_handshake.unwrap();
+    server_handshake.unwrap();
+}
